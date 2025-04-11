d1f54e89d02bc802c08b3a07780bf9a365cd8a90
dmitry.koleev <dmitry.koleev@gmail.com>
2025-04-11 22:50:43 +0300

    rotate player

--- Runtime/PlayerMovement.cs
+++ Runtime/PlayerMovement.cs
@@ -6,9 +6,12 @@ namespace Darkness.Runtime
 {
     public class PlayerMovement : MonoBehaviour
     {
+        private static readonly int IsMoving = Animator.StringToHash("IsMoving");
         [SerializeField] private float moveSpeed = 5f;
 
         private Rigidbody2D _rb;
+        private SpriteRenderer _characterSprite;
+        private Animator _characterAnimator;
 
         private InputAction _moveAction;
         private InputAction _jumpAction;
@@ -18,6 +21,8 @@ namespace Darkness.Runtime
         private void Start()
         {
             _rb = GetComponent<Rigidbody2D>();
+            _characterSprite = GetComponentInChildren<SpriteRenderer>();
+            _characterAnimator = GetComponentInChildren<Animator>();
 
             _moveAction = InputSystem.actions.FindAction("Move");
             _jumpAction = InputSystem.actions.FindAction("Jump");
@@ -35,6 +40,18 @@ namespace Darkness.Runtime
         private void FixedUpdate()
         {
             _rb.linearVelocity = _moveInput * moveSpeed;
+            if (_moveInput != Vector2.zero)
+            {
+                _characterAnimator.SetBool(IsMoving, true);
+                if (_moveInput.x != 0)
+                {
+                    _characterSprite.flipX = _moveInput.x < 0;
+                }
+            }
+            else
+            {
+                _characterAnimator.SetBool(IsMoving, false);
+            }
         }
     }
 }
