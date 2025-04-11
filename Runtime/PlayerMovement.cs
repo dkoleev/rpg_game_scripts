911225e66f353345a29e4cf8010fa634b82e72e0
dmitry.koleev <dmitry.koleev@gmail.com>
2025-04-11 20:26:11 +0300

    add basic player movement

---
+++ Runtime/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Darkness.Runtime
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 5f;
+
+        private Rigidbody2D _rb;
+
+        private InputAction _moveAction;
+        private InputAction _jumpAction;
+
+        private Vector2 _moveInput;
+
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+
+            _moveAction = InputSystem.actions.FindAction("Move");
+            _jumpAction = InputSystem.actions.FindAction("Jump");
+        }
+
+        void Update()
+        {
+            _moveInput = _moveAction.ReadValue<Vector2>();
+            if (_jumpAction.IsPressed())
+            {
+                // your jump code here
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.linearVelocity = _moveInput * moveSpeed;
+        }
+    }
+}
