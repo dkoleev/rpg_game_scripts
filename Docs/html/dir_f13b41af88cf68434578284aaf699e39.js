52dc2885f68ea08644b6664d20d231948f3df6bf
dmitry.koleev <dmitry.koleev@gmail.com>
2025-07-10 18:11:06 +0300

    remove Docs

--- Docs/html/dir_f13b41af88cf68434578284aaf699e39.js
+++
@@ -1,4 +0,0 @@
-var dir_f13b41af88cf68434578284aaf699e39 =
-[
-    [ "CodeStyle.cs", "_code_style_8cs.html", null ]
-];
--- Docs/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['codestyle_2ecs_0',['CodeStyle.cs',['../_code_style_8cs.html',1,'']]]
-];
