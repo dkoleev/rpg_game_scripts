b61b48fbf2f2de56e77e4d91ca76c3689eab3e99
dmitry.koleev <dmitry.koleev@gmail.com>
2025-08-18 09:56:28 +0300

    parallax fixes

--- Runtime/Gameplay/Effects/ParallaxBackground.cs
+++ Runtime/Gameplay/Effects/ParallaxBackground.cs
@@ -1,45 +1,41 @@
-﻿using UnityEngine;
+﻿using Alchemy.Inspector;
+using UnityEngine;
 
 namespace Darkness.Runtime.Gameplay.Effects {
     public class ParallaxBackground : MonoBehaviourExt {
-        // Far background (sky) → factor ~0.05
-        // Closer background (ruins) → factor ~0.3
-        // Foreground decor → factor ~1.2 (moves slightly faster than player)
-        [SerializeField] [Range(0f, 2f)] private float factorX = 0.5f;
-        // [SerializeField] [Range(0f, 2f)] private float factorY = 0.5f;
-        [SerializeField] private float pixelsPerUnit = 32f; // совпадает с PPU у спрайтов
+        [HelpBox("Far background (sky) → factor ~0.4\n" + 
+                 "Middle background (ruins) → factor ~0.3\n" +
+                 "Near background (ruins) → factor ~0.2\n" +
+                 "Foreground far → factor ~ -0.1 (moves slightly faster than player)\n" +
+                 "Foreground near → factor ~ -0.2 (moves more faster than player)\n")]
+        [SerializeField] private float parallaxFactor;
 
         private Transform _cam;
-        private Vector3 _lastCamPos;
+        private float _lastCamPosX;
         private bool _initialized;
 
         protected override void OnGameReady() {
             _cam = UnityEngine.Camera.main.transform;
-            _lastCamPos = _cam.position;
+            _lastCamPosX = _cam.position.x;
             _initialized = true;
         }
-        
 
-        private void LateUpdate() {
+        private void Update() {
             if (!_initialized) {
                 return;
             }
             
-            var delta = _cam.position - _lastCamPos;
-        
-            // Сдвигаем фон
-            var newPos = transform.position + new Vector3(
-                delta.x * factorX,
-                0,// delta.y * factorY,
-                0
-            );
-        
-            // Округляем к ближайшему пикселю
-            newPos.x = Mathf.Round(newPos.x * pixelsPerUnit) / pixelsPerUnit;
-            newPos.y = Mathf.Round(newPos.y * pixelsPerUnit) / pixelsPerUnit;
-        
-            transform.position = newPos;
-            _lastCamPos = _cam.position;
+            if(Mathf.Approximately(_cam.position.x, _lastCamPosX))
+            {
+                return;
+            }
+            
+            var delta = _lastCamPosX - _cam.position.x;
+            _lastCamPosX = _cam.position.x;
+            
+            var newPos = transform.localPosition;
+            newPos.x -= delta * parallaxFactor;
+            transform.localPosition = newPos;
         }
     }
 }
